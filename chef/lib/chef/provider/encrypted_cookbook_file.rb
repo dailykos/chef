73720de848ee4dfd2f85f3592c1096dc1dd427fd
ctdk <jbingham@gmail.com>
2012-04-23 13:10:02 -0700

    Filling in more encryption functions, starting actions

--- chef/lib/chef/provider/encrypted_cookbook_file.rb
+++ chef/lib/chef/provider/encrypted_cookbook_file.rb
@@ -16,15 +16,84 @@
 # limitations under the License.
 #
 
+# Borrowing quite a bit indeed from encrypted_data_bag_item here.
+
 require 'chef/file_access_control'
 require 'chef/provider/encrypted_cookbook_file'
 require 'tempfile'
+require 'openssl'
 
 class Chef
   class Provider
     class EncryptedCookbookFile < Chef::Provider::CookbookFile
+      DEFAULT_SECRET_FILE = "/etc/chef/encrypted_file_secret"
+      ALGORITHM = 'aes-256-cbc'
 
+      def load_current_resource
+        @current_resource = Chef::Resource::EncryptedCookbookFile.new(@new_resource.name)
+        @new_resource.path.gsub!(/\\/, "/") # for Windows
+        @current_resource.path(@new_resource.path)
+        @current_resource
+      end
 
+      def action_create
+
+      end
+
+      def self.load_secret(path=nil)
+	path = path || Chef::Config[:encrypted_file_secret] || DEFAULT_SECRET_FILE
+	secret = case path
+		 when /^\w+:\/\//
+		   # We have a remote key
+		   begin
+		     Kernel.open(path).read.strip
+		   rescue Errno::ECONNREFUSED
+		     raise ArgumentError, "Remote key not available from '#{path}'"
+		   rescue OpenURI::HTTPError
+		     raise ArgumentError, "Remote key not found at '#{path}'"
+		   end
+		 else
+		   if !File.exists?(path)
+		     raise Errno::ENOENT, "file not found '#{path}'"
+		   end
+		   IO.read(path).strip
+		 end
+	if secret.size < 1
+	  raise ArgumentError, "invalid zero length secret in '#{path}'"
+	end
+	secret
+      end
+
+      protected
+
+	def self.cipher(direction, data, key)
+	  cipher = OpenSSL::Cipher::Cipher.new(ALGORITHM)
+	  cipher.send(direction)
+	  cipher.pkcs5_keyivgen(key)
+	  ans = cipher.update(data)
+	  ans << cipher.final
+	  ans
+	end
+
+	def self.decrypt_file(enc_file, plain_file key)
+	  unless ::File.exists?(enc_file)
+	    raise Errno::ENOENT, "File not found: #{enc_file}"
+	  end
+	  fh = ::File.open(enc_file, "r+b")
+	  enc_data = fh.read
+	  plain_data = self.cipher(:decrypt, enc_data, key)
+	  ::File.open(plain_file, "wb"){ |f| f.write(plain_data) }
+	end
+
+	def self.encrypt_file(plain_file, enc_file, key)
+	  unless ::File.exists?(plain_file)
+	    raise Errno::ENOENT, "File not found: #{plain_file}"
+	  end
+	  fh = ::File.open(plain_file, "rb")
+	  plain_data = fh.read
+	  enc_data = self.cipher(:encrypt, plain_data, key)
+	  ::File.open(enc_file, "wb"){ |f| f.write(enc_data) }
+	end
 
     end
   end
